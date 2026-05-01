c208eb8c05b762ea7a6bcb4f203d7609b6896ba6
propeg19-boop <propeg19@gmail.com>
2026-05-01 10:23:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ Routes: /generate-plan, /generate-answer, /get-images
 """
 
 import os
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from dotenv import load_dotenv
